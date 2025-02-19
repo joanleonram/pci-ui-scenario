4335281c0f89b3de408e6a8ee43e911309e17a08
joanleonram <joanleonrs@gmail.com>
2025-02-18 23:40:15 -0500

    [Task #1] Add "Near-Earth Object Overview" title to App and browser tab

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
-import NeoGrid from './Grid';
+import NeoGrid from "./Grid";
 
 const App = () => {
   return (
+    <>
+      <h1 className="app__title">Near-Earth Object Overview</h1>
       <NeoGrid />
+    </>
   );
-}
+};
 
 export default App;
